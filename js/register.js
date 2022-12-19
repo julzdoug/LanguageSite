009726b0a3c3d578273fc5ed7f4ed80cfc06bf38
julzdoug <jschwarzzh@hotmail.com>
2022-12-19 10:50:35 +0100

    starting javascript

---
+++ js/register.js
@@ -0,0 +1,14 @@
+/*1. #inlineRadio1 = male #inlineRadio2 = female #inlineRadio3 = other?
+2.#fname= first Name lname= last Name
+3.#street= address #postCode = postal code
+4.#city= city #country= country #phoneNr= phone
+5.#dateOfBirth = date of birth #email= email
+
+Checkboxes 
+6.#english #russian #german #greek 
+7.#french #dutch #spanish #hungarian
+
+8.#bills #creditCard
+9.#termsAndConditions
+
+*/
