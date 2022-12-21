c8d105d9619d4df14bd28f344d5c64455d5fa791
julzdoug <jschwarzzh@hotmail.com>
2022-12-21 11:55:59 +0100

    start confirmation javascript

---
+++ js/my-confirmation.js
@@ -0,0 +1,31 @@
+
+// Funktion: Schlüssel und Wert in LocalStorage setzen.
+function setLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+// Funktion: Wert zum Schlüssel aus LocalStorage auslesen.
+function getLocalStorage(key) {
+    return localStorage.getItem(key);
+}
+
+// Funktion: Bestätigung generieren
+function writeConfirmation() {
+    // Die Personalien werden in Variablen ausgelesen.
+    let firstName = getLocalStorage("fname");
+    let lastName;
+    // alert(`firstName = ${firstName}`);
+
+    /*
+        Die Beschriftungen und Platzhalter für die obigen Variablen werden in ein 
+        String Template eingetragen. Zur Gliederung und Gestaltung werden zudem 
+        <p> und <span> Elemente verwendet.
+    */
+    let confirmation = `
+        <p><span class="confirmation-label">Vorname:</span> ${firstName}</p>
+        <p><span class="confirmation-label">Nachname:</span> ${lastName}</p>
+    `;
+
+    // Die Bestätigung wird als innerHTML gesetzt.
+    document.getElementById("confirmation-message").innerHTML = confirmation;
+}
